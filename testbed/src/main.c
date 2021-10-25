27751fb17f2473eff90f3c7530d9cf594501c53c
Elise <elisefleur.v@gmail.com>
2021-10-25 19:31:43 +0200

    initial commit, log, asserts, windows

---
+++ testbed/src/main.c
@@ -0,0 +1,16 @@
+#include<core/logger.h>
+#include<core/asserts.h>
+
+
+int main(void){
+    KFATAL("a test message: %f", 3.14f);
+    KERROR("a test message: %f", 3.14f);
+    KWARN("a test message: %f", 3.14f);
+    KINFO("a test message: %f", 3.14f);
+    KDEBUG("a test message: %f", 3.14f);
+    KTRACE("a test message: %f", 3.14f);
+
+    KASSERT(1==0)
+    
+    return 0;
+}
